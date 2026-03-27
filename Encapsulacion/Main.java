192b8709883558a8f2cced512e2b925030dfdf1e
Antony Najarro <anajarrom@miumg.edu.gt>
2026-03-26 21:03:08 -0600

    ClasePersona

---
+++ Encapsulacion/Main.java
@@ -0,0 +1,18 @@
+package Encapsulacion;
+
+import Main.java.Persona;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Persona prs1 = new Persona("Antony","Najarro",20);
+		Persona prs2 = new Persona("David","Lopez",23);
+		Persona prs3 = new Persona("Fernanda","Aguilar",60);
+			System.out.println("------------Personas------------");
+		prs1.Mostrar();
+		prs2.Mostrar();
+		prs3.Mostrar();
+	}
+	
+}
---
+++ Encapsulacion/Persona.java
@@ -0,0 +1,20 @@
+package Encapsulacion;
+
+public class Persona {
+
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	public Persona(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+	 
+	public void Mostrar() {
+		System.out.println("Informacion:" + " Nombre:" + nombre + " Apellido:"  + apellido + " Edad:"  + edad);
+	}
+
+}
